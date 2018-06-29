9454962820d3a420fc7daaefdb3743059dc9e94b
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-06-29 10:44:53 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,9 @@ class CreateShows < ActiveRecord::Migrate
   def change  
     create_table :shows do |t|
       t.string          :name
-      
+      t.string          :network
+      t.string          :day
+      t.integer         :rating
       t.timestamps
     end
   end
